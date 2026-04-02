00ec0c45aa9fa3f8757818ec1ca4d364dc182a0d
PECAUSA240 <davidjacome43@gmail.com>
2026-04-02 10:05:57 -0500

    Clase nueva

---
+++ 01_basic/clase09_funciones_y_asignacion_variables.py
@@ -0,0 +1,15 @@
+###
+# FUNCIONES
+# Son un bloque de código que lo podemos reutilizar más adelante
+# estos bloques de código pueden depender de ciertos parámetros o no
+# Ademas, las funciones no siempre tendran que retornar un valor
+###
+
+### SINTÁXIS
+# -> la palabra 'def' es para crear la funcion, 
+# luego va el nombre de la funcion y lo que va dentro 
+# del parentesis son los parametros de los cuales depende la funcion
+def mi_funcion(parametro1 , parametro2): 
+    # cuerpo de la funcion
+    resultado = parametro1 + parametro2
+    return resultado
