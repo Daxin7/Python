c52cb704a540c57f197ccb4e50694b65989a5ab9
PECAUSA240 <davidjacome43@gmail.com>
2026-04-06 20:16:57 -0500

    Finalizacion de clase

--- 01_basic/clase09_funciones_y_asignacion_variables.py
+++ 01_basic/clase09_funciones_y_asignacion_variables.py
@@ -9,7 +9,47 @@
 # -> la palabra 'def' es para crear la funcion, 
 # luego va el nombre de la funcion y lo que va dentro 
 # del parentesis son los parametros de los cuales depende la funcion
+# IMPORTANTE:
+# Si una funcion no lleva parametros se les denominan funcniones anonimas
 def mi_funcion(parametro1 , parametro2): 
     # cuerpo de la funcion
     resultado = parametro1 + parametro2
     return resultado
+
+print(mi_funcion(3 , 5)) #-> de esta manera hacemos uso de la funcion
+
+
+print("**********")
+# otro ejemplo
+
+def Saludar(saludo, nombre):
+    print(f"{saludo} {nombre}!")
+
+print(Saludar("Hola" , "David"))
+print("**********")
+
+# tambien podemos predefinir los valores de los parametros en la funcion
+def Saludar1(nombre, saludo1="HOLA"):
+    print(f"{saludo1} , {nombre}")
+
+print(Saludar1("DAVID"))
+print("**********")
+
+# otro ejemplo de funcion
+def cuadrado(x):
+    result = x ** 2
+    return result
+
+print(cuadrado(4))
+print("**********")
+
+# Ejemplos de funcniones anonimas
+
+def Imprimir():
+    print("Hola Mundo!")
+
+resultado = Imprimir()
+
+print(resultado)#-> aqui imprime None ya que la funcion Imprimir no retorna ningun valor
+
+print("**********")
